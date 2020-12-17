08d5b8beaddd1ed21072b4b2ade1aaa2a20029ba
Painted-Black <syzygy.sys@gmail.com>
2020-12-17 16:41:44 +0300

    Test unlink

--- source/tests/unlink_test/main.c
+++ source/tests/unlink_test/main.c
@@ -5,10 +5,15 @@
 #include <unistd.h>
 
 #define FILE2 "/home/lander/BMSTU-OS-Course-Project/source/tests/unlink_test_file"
+#define FILE1 "/home/lander/BMSTU-OS-Course-Project/source/tests/unlink_test_dir"
 
 int main(void)
 {
-
     int fd = unlink(FILE2);
-    printf("unlink() returned %d\n", fd);
+    printf("unlink() file returned %d\n", fd);
+
+    int fd2 = unlink(FILE1);
+    printf("unlink() dir returned %d\n", fd2);
+
+    return 0;
 }
